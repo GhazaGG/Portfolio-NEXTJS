c785f8664f416fb6e331c1fbef6518c0c211a50f
defiandriani <ghazagg7@gmail.com>
2024-09-23 22:47:10 +0700

    fix footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero";
 import About from "@/components/About"
 import Project from "@/components/Project";
 import Skills from "@/components/Skills"
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Footer = () => {
+const footer = () => {
   return (
     <div className='text-center py-10 font-extralight bg-black text-[12px]'>
-      <p>Built by GhazaGG.</p>
-      <p>integrating Aceternity UI and powered by Next.js</p>
-    </div>
+        <p>Built by GhazaGG.</p>
+        <p>integrating Aceternity UI and powered by Next.js</p>
+  </div>
   )
 }
 
-export default Footer
+export default footer
